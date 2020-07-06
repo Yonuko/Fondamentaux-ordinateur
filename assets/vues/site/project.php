a7527803f491cc03fd07f56548a9ea4d04ffc387
Sacha <sacha.eghiazarian@ynov.com>
2020-07-06 19:28:19 +0200

    Debut de la creation de la page projet

--- assets/vues/site/project.php
+++ assets/vues/site/project.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://localhost/portfolio/assets/style/Site/main.css">
+    <link rel="stylesheet" href="http://localhost/portfolio/assets/style/Site/project.css">
     <?php 
         $rqt = "SELECT * FROM projects WHERE project_id = ?;";
         $project = sendRequest($rqt, [$id], PDO::FETCH_ASSOC)[0];
@@ -34,6 +35,25 @@
     </header>
     <main>
         <?php
+            $rqt = "SELECT * FROM projects WHERE project_id = ?;";
+            $project = sendRequest($rqt, [$id], PDO::FETCH_ASSOC);
+            extract($project);
+            echo "
+            <section class='project-presentation' 
+            style=\"background-image: url('http://localhost/portfolio/assets/image/Uploads/Projets/$logo');\">
+                <h1>$name</h1>
+                <section class='message'>
+                    <span>Bonjour !</span>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, aut obcaecati! <br>Cupiditate quas deleniti officia ad ea aperiam porro perferendis atque dolorum <br>consectetur assumenda, placeat vitae, unde perspiciatis quasi doloribus?
+                        <br><br>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora in ex a autem deserunt voluptatem repellendus laudantium beatae libero hic, reprehenderit sequi, illum magnam iusto pariatur officiis quam eum. Possimus?
+                    </p>
+                </section>
+            </section>
+            ";
+        ?>
+        <?php
             $rqt = "SELECT * FROM projects_description WHERE project_id = ? ORDER BY `order`;";
             $descTab = sendRequest($rqt, [$id], PDO::FETCH_ASSOC);
             if(!is_null($descTab)){
@@ -44,18 +64,27 @@
                 }
             }
         ?>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
+        <section class="project">
+            <h2>Mes projets</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi vel cumque iusto consectetur natus nostrum provident voluptatem, saepe fugit sit laboriosam consequuntur possimus doloremque fuga sed architecto, voluptatum rem ullam!</p>
+            <a class="button" href="http://localhost/portfolio/projects">Mes projets</a>
+            <div class="project-list">
+                <?php 
+                    $rqt = "SELECT * from projects WHERE isShown = 1 LIMIT 3;";
+                    $projects = sendRequest($rqt, [], PDO::FETCH_ASSOC);
+                    foreach($projects as $project){
+                        extract($project);
+                        echo "
+                        <div class='project-item' onclick=\"location.href = 'http://localhost/portfolio/projects/$project_id'\">
+                            <span class='image' 
+                            style=\"background-image: url('http://localhost/portfolio/assets/image/Uploads/Projets/$logo');\"></span>
+                            <p>$name</p>
+                        </div>
+                        ";
+                    }
+                ?>
+            </div>
+        </section>
     </main>
     <footer>
         <div class="menu">
