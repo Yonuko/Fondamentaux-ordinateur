3ea8cb73571b077ea77455d066ae910050e12bf9
Sacha <sacha.eghiazarian@ynov.com>
2020-07-06 18:35:35 +0200

    Ajout de la page Projets

--- assets/vues/site/projects.php
+++ assets/vues/site/projects.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://localhost/portfolio/assets/style/Site/main.css">
+    <link rel="stylesheet" href="http://localhost/portfolio/assets/style/Site/projects.css">
     <title>Portfolio Sacha EGHIAZARIAN - Projets</title>
 </head>
 <body>
@@ -21,6 +22,77 @@
             ?>
         </div>
     </header>
+    <main>
+    <section class="title">
+            <h1>Mes projets</h1>
+        </section>
+        <section class="Latest-project">
+            <?php 
+                $rqt = "SELECT * FROM projects ORDER BY project_id DESC LIMIT 1;";
+                $latest_project = sendRequest($rqt, [], PDO::FETCH_ASSOC)[0];
+                extract($latest_project);
+                $rqt = "SELECT Description FROM projects_description WHERE `order` = 1 AND project_id = ?;";
+                $desc = sendRequest($rqt, [$project_id], PDO::FETCH_NUM)[0][0];
+                echo "
+                <span class='latest-project-image' 
+                style=\"background-image: url('http://localhost/portfolio/assets/image/Uploads/Projets/$logo')\"></span>
+                <div class='latest-project-message'>
+                    <span>$name</span>
+                    <div class='description'>" . html_entity_decode($desc) . "</div>
+                    <a href='http://localhost/portfolio/projects/$project_id' class='button'>Voir le projet</a>
+                </div>
+                ";
+            ?>
+        </section>
+        <section class="projects">
+            <div class='project-list'>
+            <?php 
+                $rqt = "SELECT * FROM projects WHERE isShown = 1;";
+                $projects = sendRequest($rqt, [], PDO::FETCH_ASSOC);
+                $count = 0;
+                foreach($projects as $project){
+                    extract($project);
+                    $rqt = "SELECT * FROM projects_description WHERE project_id = ? AND `order` = 1;";
+                    $desc = sendRequest($rqt, [$project_id], PDO::FETCH_ASSOC)[0];
+                    extract($desc);
+                    if($count % 3 === 0 && $count !== 0){
+                        echo "</div><div class='project-list'>";
+                    }
+                    echo "
+                    <div class='project' onclick=\"location.href = 'http://localhost/portfolio/projects/$project_id'\">
+                        <span class='project-icon' 
+                        style=\"background-image: url('http://localhost/portfolio/assets/image/Uploads/Projets/$logo');\"></span>
+                        <span class='project-title'>$name</span>
+                        <div class='description'>" . html_entity_decode($Description) . "</div>
+                    </div>
+                    ";
+                    $count++;
+                }
+            ?>
+            </div>
+        </section>
+        <section class="posts">
+            <h2>Mes articles</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi vel cumque iusto consectetur natus nostrum provident voluptatem, saepe fugit sit laboriosam consequuntur possimus doloremque fuga sed architecto, voluptatum rem ullam!</p>
+            <a class="button" href="http://localhost/portfolio/projects">Mon blog</a>
+            <div class="posts-list">
+                <?php 
+                    $rqt = "SELECT * from projects WHERE isShown = 1 LIMIT 3;";
+                    $projects = sendRequest($rqt, [], PDO::FETCH_ASSOC);
+                    foreach($projects as $project){
+                        extract($project);
+                        echo "
+                        <div class='posts-item' onclick=\"location.href = 'http://localhost/portfolio/projects/$project_id'\">
+                            <span class='image' 
+                            style=\"background-image: url('http://localhost/portfolio/assets/image/Uploads/Projets/$logo');\"></span>
+                            <p>$name</p>
+                        </div>
+                        ";
+                    }
+                ?>
+            </div>
+        </section>
+    </main>
     <footer>
         <div class="menu">
             <a href="http://localhost/portfolio">Accueil</a>
