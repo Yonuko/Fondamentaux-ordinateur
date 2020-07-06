fe2d09e50d8bef30adfddaaa52bd82ac7903e18f
Sacha <sacha.eghiazarian@ynov.com>
2020-07-06 15:05:52 +0200

    ajout de la page contact

--- assets/vues/site/contact.php
+++ assets/vues/site/contact.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://localhost/portfolio/assets/style/Site/main.css">
+    <link rel="stylesheet" href="http://localhost/portfolio/assets/style/Site/contact.css">
     <title>Portfolio Sacha EGHIAZARIAN - Contact</title>
 </head>
 <body>
@@ -21,6 +22,42 @@
             ?>
         </div>
     </header>
+    <main>
+        <section class="contact">
+            <div class="message">
+                <p>Me contacter</p>
+            </div>
+            <div class="contact-list">
+                <p>mail :</p>
+                <a href="mailto:sacha.eghiazarian@ynov.com">sacha.eghiazarian@ynov.com</a>
+                <p>Téléphone :</p>
+                <p class="phone">06 95 20 97 55</p>
+                <p>LinkedIn :</p>
+                <a href="https://www.linkedin.com/in/sacha-eghiazarian/">linkedin.com/sacha-eghiazarian/</a>
+            </div>
+        </section>
+        <section class="project">
+            <h2>Mes projets</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi vel cumque iusto consectetur natus nostrum provident voluptatem, saepe fugit sit laboriosam consequuntur possimus doloremque fuga sed architecto, voluptatum rem ullam!</p>
+            <a class="button" href="http://localhost/portfolio/projects">Mes projets</a>
+            <div class="project-list">
+                <?php 
+                    $rqt = "SELECT * from projects WHERE isShown = 1 LIMIT 3;";
+                    $projects = sendRequest($rqt, [], PDO::FETCH_ASSOC);
+                    foreach($projects as $project){
+                        extract($project);
+                        echo "
+                        <div class='project-item' onclick=\"location.href = 'http://localhost/portfolio/projects/$project_id'\">
+                            <span class='image' 
+                            style=\"background-image: url('http://localhost/portfolio/assets/image/Uploads/Projets/$logo');\"></span>
+                            <p>$name</p>
+                        </div>
+                        ";
+                    }
+                ?>
+            </div>
+        </section>
+    </main>
     <footer>
         <div class="menu">
             <a href="http://localhost/portfolio">Accueil</a>
